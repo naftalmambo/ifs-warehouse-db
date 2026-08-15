9870b0b89d8f4b3823f4fce633f5c23544cbd66c
Naftal <naftalmmb@gmail.com>
2026-08-15 14:09:21 +0300

    Add columns to product table

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,4 +1,3 @@
-DROP TABLE inventory_ledger;
 
 
 CREATE TABLE suppliers(
@@ -7,9 +6,11 @@ CREATE TABLE suppliers(
 );
 
 CREATE TABLE products(
-
-
-
+    Product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(150) UNIQUE NOT NULL,
+    description TEXT,
+    unit_price NUMERIC(10, 2) NOT NULL CHECK (unit_price > 0),
+    stock_level INT NOT NULL DEFAULT 0 CHECK (stock_level >= 0)
 );
 
 CREATE TABLE purchase_orders(
