39aabfb5404223b91392fdea4de654c5042f1d0b
Naftal <naftalmmb@gmail.com>
2026-08-17 00:09:16 +0300

    Import java.sql database connection and data aggregation libraries

--- src/WarehouseApp.java
+++ src/WarehouseApp.java
@@ -1,3 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class WarehouseApp {
 
     public static void main(String[] args) {
